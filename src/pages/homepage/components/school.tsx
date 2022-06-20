b45b6adf6eb4ae03a1c876b710edca7fe53cc3e0
constanzadl <constanzadl94@gmail.com>
2022-06-20 15:57:33 -0600

    layout and about me

--- src/pages/homepage/components/school.tsx
+++ src/pages/homepage/components/school.tsx
@@ -4,16 +4,12 @@ interface ExpProps {
   degree: string
 }
 
-export const Experience = ({
-  school: string,
-  date: string,
-  degree: string
-}) => {
+export const Experience = (props: ExpProps) => {
   return (
     <div className="pt-4 border-t-2 border-white lg:border-none">
-      <div className="text-lg">{school}</div>
-      <div className="text-base">{date}</div>
-      <div className="text-3xl">{degree}</div>
+      <div className="text-lg">{props.school}</div>
+      <div className="text-base">{props.date}</div>
+      <div className="text-3xl">{props.degree}</div>
     </div>
   )
 }
