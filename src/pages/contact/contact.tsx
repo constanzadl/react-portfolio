a9969e58bbd522268af0f0c4fd4b07157227252c
constanzadl <constanzadl94@gmail.com>
2022-06-27 11:32:59 -0500

    contact page

--- src/pages/contact/contact.tsx
+++ src/pages/contact/contact.tsx
@@ -1,3 +1,22 @@
 export const Contact = () => {
-  return <div>Contact</div>
+  return (
+    <div className="flex justify-center p-16 w-full text-white">
+      <a
+        href="https://github.com/constanzadl"
+        className="hover:text-violet-600"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="m-4 text-6xl fab fa-github"></i>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/constanza-duarte-4b0a957a/"
+        className="hover:text-violet-600"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="m-4 text-6xl fab fa-linkedin"></i>
+      </a>
+    </div>
+  )
 }
