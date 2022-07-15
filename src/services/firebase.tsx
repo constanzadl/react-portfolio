456fafe791b6712d9c7cf6087e4511df8f0a477f
constanzadl <constanzadl94@gmail.com>
2022-07-15 15:09:12 -0500

    hidden variables

--- src/services/firebase.tsx
+++ src/services/firebase.tsx
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
 import { getAnalytics } from 'firebase/analytics'
-import { getFirestore } from 'firebase/firestore'
+import { getFirestore } from 'firebase/firestore/lite'
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -9,13 +9,13 @@ import { getFirestore } from 'firebase/firestore'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyD70SBkM47G26kelb92EcQOn8wIe50ZRAU',
-  authDomain: 'portfolio-db-107fc.firebaseapp.com',
-  projectId: 'portfolio-db-107fc',
-  storageBucket: 'portfolio-db-107fc.appspot.com',
-  messagingSenderId: '493058159240',
-  appId: '1:493058159240:web:9bca4bfe754804bac0253d',
-  measurementId: 'G-VWWR8K2R8N'
+  apiKey: import.meta.env.VITE_REACT_APP_API_KEY,
+  authDomain: import.meta.env.VITE_REACT_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_REACT_APP_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_REACT_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_REACT_APP_APP_ID,
+  measurementId: import.meta.env.VITE_REACT_APP_MEASUREMENT_ID
 }
 
 // Initialize Firebase
